00ff9a31588f31ae31143fd82919fc9133d32037
Takamitsu Shirono <takshiro@bu.edu>
2020-12-10 09:42:37 +0900

    update data

--- main/aggregate_json.py
+++ main/aggregate_json.py
@@ -7,21 +7,19 @@ def aggregate_json(ticker):
 	prev_results = os.listdir(JSON_PATH)
 	prev_results = [x for x in prev_results if (x.endswith(".json") and x[:-5].isdigit())]
 
-	count = 0
 	res = {}
 	for json_filename in prev_results:
 		json_filepath = os.path.join(JSON_PATH, json_filename)
+		json_filekey = int(json_filename[:-5]) - 1
+
 		try:
 			with open(json_filepath) as json_file:
 				data = json.load(json_file)
 		except:
 			print(json_filepath)
 
-		res[str(count)] = data
-		count += 1
+		res[str(json_filekey)] = data
 
 	SAVE_PATH = os.path.join(JSON_PATH, "master.json")
 	with open(SAVE_PATH, 'w') as outfile:
 			json.dump(res, outfile)
-
-	print(count)
